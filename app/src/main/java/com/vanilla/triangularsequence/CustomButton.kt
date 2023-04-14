1fd917a92db9d9f0ae49a8dff35ef973c05fd8c4
Ram Mandal <ram.mandal@8squarei.com>
2023-04-15 00:58:11 +0545

    Configured

---
+++ app/src/main/java/com/vanilla/triangularsequence/CustomButton.kt
@@ -0,0 +1,51 @@
+package com.vanilla.triangularsequence
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
+
+/**
+ * Created by Ram Mandal on 21/03/2023
+ * @System: Apple M1 Pro
+ */
+class CustomButton : Button, View.OnClickListener {
+    constructor(context: Context?) : super(context) {
+        init(null)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init(attrs)
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(attrs)
+    }
+
+    private fun init(attrs: AttributeSet?) {
+        setOnClickListener(this)
+    }
+
+    private var mUserOnClickListener: OnClickListener? = null
+
+    override fun setOnClickListener(l: OnClickListener?) {
+        if (l == this)
+            super.setOnClickListener(l)
+        else mUserOnClickListener = l
+    }
+
+    override fun onClick(p0: View?) {
+        Log.v("TAG", "Clicked")
+        if (mUserOnClickListener != null) {
+            mUserOnClickListener!!.onClick(p0)
+        }
+    }
+
+
+}
